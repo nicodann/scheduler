79284afe22f84c7cba5f205516d3219e262a4a66
Nico <nicodann@gmail.com>
2022-03-22 10:47:46 -0400

    install cypress

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
+// IT RENDERS
+xit("renders without crashing", () => {
   render(<Application />);
 });
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday"));
+})
+
+//AXIOS FUNCTIONS
+
+//SCHEDULER CAN LOAD DATA
+
+//ASYNCH TEST WAITS FOR COMPONENT TO UPDATE
+
+//USE CONTAINERS TO FIND SPECIFIC DOM NODES
+
+//CHAIN PROMISES TO HANDLE ASYNCH TESTING
+
+// OVERRIDE MOCK IMPLEMENTATIONS
+
+//SETUP AND TEARDOWN FUNCITONS TO PERFORM COMMON TASKS
